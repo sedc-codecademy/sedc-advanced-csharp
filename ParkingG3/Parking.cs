95bc5309cc341aa0ccc42d258ff4985166804407
Wekoslav Stefanovski <swekster@gmail.com>
2017-03-18 15:00:06 +0100

    parking g3 - initial

---
+++ ParkingG3/Parking.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingG3
+{
+    class Parking<T>
+    {
+        private Queue<T> storage;
+
+        public int Capacity { get; private set; }
+
+        public Parking(int capacity)
+        {
+            Capacity = capacity;
+            storage = new Queue<T>();
+        }
+
+        public override string ToString()
+        {
+            return $"I'm a parking with a capacity of {Capacity}";
+        }
+
+        public bool IsFull()
+        {
+            return false;
+        }
+
+        public bool HasVehicles()
+        {
+            return storage.Count == 0;
+        }
+
+        public void Park(T vehicle)
+        {
+            if (storage.Count == Capacity)
+            {
+                throw new InvalidOperationException("Parking is full");
+            }
+            storage.Enqueue(vehicle);
+        }
+
+        public T LeaveParking()
+        {
+            if (storage.Count == 0)
+            {
+                throw new InvalidOperationException("Parking is empty");
+            }
+            return storage.Dequeue();
+        }
+
+    }
+}
--- ParkingG3/Program.cs
+++ ParkingG3/Program.cs
@@ -10,9 +10,46 @@ namespace ParkingG3
     {
         static void Main(string[] args)
         {
-            var parking = new Parking<string> (20);
+            TestParking<string>(20, new List<string>
+            {
+                "Audi",
+                "Mercedes",
+                "Toyota",
+                "Ferarri",
+                "Yugo",
+                "Ford",
+                "BMW",
+                "Opel"
+            });
+
+            TestParking<int>(50, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+        }
+
+        static void TestParking<T>(int capacity, List<T> values)
+        {
+            Console.WriteLine($"--- {typeof(T)} parking with {capacity} capacity");
+            var parking = new Parking<T>(capacity);
+            try
+            {
+                parking.LeaveParking();
+                Console.WriteLine("YOU SHOULD NOT SEE ME");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            parking.Park(values[0]);
+            parking.Park(values[1]);
+            parking.Park(values[2]);
+
+            var result = parking.LeaveParking();
+            Console.WriteLine($"result is {result}");
+            result = parking.LeaveParking();
+            Console.WriteLine($"result is {result}");
 
             Console.WriteLine(parking);
+            Console.WriteLine(parking.Capacity);
         }
     }
 }
