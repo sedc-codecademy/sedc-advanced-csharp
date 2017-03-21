5db8ce7ca9c9abce43b82c2d63334767dbcacaab
Wekoslav Stefanovski <swekster@gmail.com>
2017-03-21 20:46:58 +0100

    mobster exercise

--- G1-Interface/Program.cs
+++ G1-Interface/Program.cs
@@ -22,21 +22,43 @@ namespace G1_Interface
             b.Shave("Petar");
             b.Makeover("Aneta");
 
+            Grocer g = new Grocer("Giorgio");
+            g.Sell("eggs");
+            g.Sell("milk");
+
             Console.WriteLine(b.Balance);
 
             Mobster tony = new Mobster("Tony", "Knife");
             tony.AddClient(p);
             tony.AddClient(b);
+            tony.AddClient(g);
 
             Console.WriteLine($"The parking has {p.Balance} denars");
             Console.WriteLine($"{b.Name} has {b.Balance} denars");
+            Console.WriteLine($"{g.Name} has {g.Balance} denars");
             Console.WriteLine($"{tony.Name} the {tony.Nickname} has {tony.Balance} denars");
 
             tony.TakeCareOfBusiness();
 
             Console.WriteLine($"The parking has {p.Balance} denars");
             Console.WriteLine($"{b.Name} has {b.Balance} denars");
+            Console.WriteLine($"{g.Name} has {g.Balance} denars");
             Console.WriteLine($"{tony.Name} the {tony.Nickname} has {tony.Balance} denars");
+
+            Mobster vini = new Mobster("Vini", "Nose");
+            var b2 = new Barber("Sergey");
+            // ...
+            var g2 = new Grocer("Peter");
+            // .. tcb
+
+
+            Mobster vito = new Mobster("Vito", "Big");
+            vito.AddClient(tony);
+            vito.AddClient(vini);
+
+            // .. tcb
+
+
 
             //IParking<string> parking = GetMeAParking();
             //parking.Park("Audi");
