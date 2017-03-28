a8271db7192929476fc041ba06f2c256102bcefd
Wekoslav Stefanovski <swekster@gmail.com>
2017-03-28 20:08:55 +0200

    added json.net

---
+++ G1-Bookr-Parser/JsonParser.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1_Bookr_Parser
+{
+    public class JsonParser
+    {
+        public IEnumerable<Author> ParseAuthors(string json)
+        {
+            List<Author> authors = JsonConvert.DeserializeObject<List<Author>>(json);
+            return authors;
+        }
+    }
+}
